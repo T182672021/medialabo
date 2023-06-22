f74a2fe1d5ff5112dc24d53a1018dece62baafea
T182672021 <218267@st.takushoku-u.ac.jp>
2023-06-22 17:15:22 +0900

    kazuate.js追加

--- kazuate.js
+++ kazuate.js
@@ -2,7 +2,7 @@
 let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
-// 入力回数（予想回数）
+// 入力回数（予想もっともっと
 let kaisu = 0;
 
 // 予想を4回実行する
@@ -17,8 +17,23 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
+  kaisu++;
   
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
+
+  console.log(kaisu + "回目の予想 : " + yoso);
+  if(kaisu >= 4){
+    console.log("答えは" + kotae + "でした.　すでにゲームは終わっています");
+  } else{
+    if(yoso == kotae){
+        console.log("正解です.おめでとう!");
+        kaisu = 4;
+    } else if(yoso < kotae){
+        console.log("まちがい.　答えはもっと大きいですよ");
+    } else if(yoso > kotae){
+        console.log("まちがい.　答えはもっと大きいですよ");
+    }
+  }
 }
